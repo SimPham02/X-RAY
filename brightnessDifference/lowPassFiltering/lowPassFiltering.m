5fe8c5ff8b41967faff533a33906d69c0527f314
SimPham02 <phamdinhtungsim@gmail.com>
2024-01-04 16:04:30 +0700

    upload lowPassFiltering

---
+++ brightnessDifference/lowPassFiltering/lowPassFiltering.m
@@ -0,0 +1,36 @@
+folder_path = '../../img/';
+
+% lấy danh sách ảnh
+img_files = dir(fullfile(folder_path, '*.tiff'));
+
+for i = 1:numel(img_files)
+    img_path = fullfile(folder_path, img_files(i).name);
+    img = imread(img_path);
+
+    % Tạo bộ lọc trung bình
+    filter = fspecial('gaussian', [7 7], 2);
+    
+    % Áp dụng bộ lọc trung bình để tăng độ sáng
+    output_image = imfilter(img, filter);
+    output_image = output_image * 3.5;
+    
+    % Hiển thị ảnh gốc và ảnh đã xử lý
+    figure;
+    subplot(2, 1, 1);
+    imshow(img);
+    title('Ảnh Gốc');
+    
+    subplot(2, 1, 2);
+    imshow(output_image);
+    title('Ảnh Sau Low-pass Filtering');
+    
+    % Chuyển đổi ảnh về kiểu dữ liệu uint8
+    img_histeq_uint8 = im2uint8(output_image);
+
+    % Tạo tên tệp mới để lưu ảnh đã chuẩn hóa histogram
+    [~, name, ext] = fileparts(img_files(i).name);
+    new_filename = [name '_histeq' ext];
+
+    % Lưu ảnh đã chuẩn hóa histogram
+    imwrite(img_histeq_uint8, fullfile('result', new_filename));
+end
